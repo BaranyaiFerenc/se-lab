9945fa813bced8f7ad564c3624d0ad4f1dd7a0e5
Baranyai Ferenc <baranyai.fer@gmail.com>
2024-10-16 09:06:20 +0200

    Kért változtatások

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -27,7 +27,7 @@ public class TorpedoStore {
       }
     }
   }
-
+  ///Hello Világ
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
